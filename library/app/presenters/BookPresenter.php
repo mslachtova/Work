fba4c102c78c62419c0e296a888923861e9a0cc7
Monika Slachtova <moncasla@email.cz>
2019-04-11 16:17:28 +0200

    Added renderShow in BookPresenter with comments

--- library/app/presenters/BookPresenter.php
+++ library/app/presenters/BookPresenter.php
@@ -4,11 +4,13 @@ namespace App\Presenters;
 use Nette;
 use App\Model\BookManager;
 use Nette\Application\UI\Form;
+use App\Model\CommentManager;
 
 
 class BookPresenter extends Nette\Application\UI\Presenter
 {
     private $bookManager;
+    private $commentManager;
     
     public function injectBookManager(BookManager $bookManager)
     {
@@ -16,9 +18,48 @@ class BookPresenter extends Nette\Application\UI\Presenter
 
     }
     
+    public function injectCommentManager(CommentManager $commentManager)
+    {
+        $this->commentManager = $commentManager;
+        
+    }
+    
     public function renderDefault()
     {
         $this->template->books = $this->bookManager->getAll();
+    }
+    
+    public function renderShow($bookId)
+    {
+        $book = $this->bookManager->find($bookId);
+        $this->template->book = $book;
+        $this->template->comments = $book->related('comment')->order('created_at');
+    }
+    
+    protected function createComponentCommentForm()
+    {
+        $form = new Form;
+        $form->addText('id', 'Identifikační číslo:')
+        ->setRequired();
+        $form->addTextArea('content', 'Komentář:')
+        ->setRequired();
+        $form->addSubmit('send', 'Zveřejnit komentář');
+        $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+        return $form;
+    }
+    
+    public function commentFormSucceeded(Form $form, \stdClass $values)
+    {
+        $bookId = $this->getParameter('bookId');
+        
+        $this->commentManager->create([
+            'customer_id' => $values->id,
+            'book_id' => $bookId,
+            'content' => $values->content,
+        ]);
+        
+        $this->flashMessage('Děkuji za komentář', 'success');
+        $this->redirect('this');
     }
 
 }
